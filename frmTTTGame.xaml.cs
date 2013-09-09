268d52509927722ff97e12a665628433449ac8ef
jkelleher_its <kelleher.john@itsligo.ie>
2013-09-09 11:11:02 +0100

    Initial commit

---
+++ frmTTTGame.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tictactoe
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        tttBoard board;
+        bool xTurn;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem mnu = (MenuItem)sender;
+            initBoard(Int32.Parse((mnu.Tag.ToString())));
+            xTurn = true;
+        }
+
+        private void initBoard(int rank)
+        {
+            Button newButton;
+            board = new tttBoard(rank);
+            boardGrid.Children.Clear();
+            boardGrid.Rows = rank;
+            for (int i=0; i<rank; i++)
+                for (int j=0; j<rank; j++)
+                {
+                    newButton = new Button();
+                    newButton.Tag = new Point(i,j);
+                    newButton.Content = newButton.Tag;
+                    boardGrid.Children.Add(newButton);
+                }
+        }
+
+        private void boardGrid_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = e.OriginalSource as Button;
+            if (b == null) return;  // reject if not a Button
+            
+            Point p = new Point();
+            p = (Point)b.Tag;       // grab Button index from Tag
+
+            if (xTurn)      // handle play for x
+            {
+                b.Content = "X";
+                board[(int)p.X, (int)p.Y] = tictactoe.tttBoard.boardState.x;
+            }
+            else
+            {
+                b.Content = "O";
+                board[(int)p.X, (int)p.Y] = tttBoard.boardState.o;
+            }
+            b.IsEnabled = false;
+            tttBoard.boardState result = board.testWin();
+            if (result == tttBoard.boardState.x)
+                MessageBox.Show("X Wins!");
+            if (result == tttBoard.boardState.o)
+                MessageBox.Show("O Wins!");
+            xTurn = !xTurn;
+
+        }
+
+        private void mnuQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }   // end class 
+}
---
+++ tttBoard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace tictactoe
+{
+    class tttBoard
+    {
+        public enum boardState {empty, x, o};
+        private boardState [,]board;
+
+        public tttBoard(int rank)
+        {
+            board =  new boardState[rank,rank];
+
+        }   // end constructor
+
+
+        public boardState this[int i, int j]
+        {
+            get
+            {
+                return board[i, j];
+            }
+            set
+            {
+                board[i, j] = value;
+            }
+        }
+
+        public boardState testWin()
+        {
+            int noX=0, noO=0;
+            int rank = board.GetLength(0);
+
+            #region // test rows
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    if (board[i, j] == boardState.x) ++noX;
+                    if (board[i, j] == boardState.o) ++noO;
+                }   // across a row
+                if (noX != rank) noX = 0; else break;
+                if (noO != rank) noO = 0; else break;
+            }   // down through rows on board
+            
+            // test if any winners in the rows
+            if (noX == rank) return boardState.x;
+            if (noO == rank) return boardState.o;
+            #endregion
+
+            #region // test columns
+            noX = noO = 0;
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    if (board[j, i] == boardState.x) ++noX;
+                    if (board[j, i] == boardState.o) ++noO;
+                }   // across a cols
+                if (noX != rank) noX = 0; else break;
+                if (noO != rank) noO = 0; else break;
+            }   // down through cols on board
+
+            // test if any winners in the cols
+            if (noX == rank) return boardState.x;
+            if (noO == rank) return boardState.o;
+            #endregion
+
+            #region // test diagonal top left to bottom right
+            noX = noO = 0;
+            for (int i = 0; i < rank; i++)
+            {
+                if (board[i, i] == boardState.x) ++noX;
+                if (board[i, i] == boardState.o) ++noO;
+            }
+
+            // test if any winners in this diagonal
+            if (noX == rank) return boardState.x;
+            if (noO == rank) return boardState.o;
+            #endregion
+
+            #region //Test diagonal bottom left to top right
+            noX = noO = 0;
+            for (int i = rank-1; i > 0; i--)
+            {
+                if (board[i, i] == boardState.x) ++noX;
+                if (board[i, i] == boardState.o) ++noO;
+            }
+
+            // test if any winners in this diagonal
+            if (noX == rank) return boardState.x;
+            if (noO == rank) return boardState.o;
+            #endregion
+
+            return boardState.empty;
+        }   // end testWin()
+    }   // end class tttBoard
+}   // end namespace
